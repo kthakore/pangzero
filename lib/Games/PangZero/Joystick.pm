e9ece2faa8eeebec472c84e22ac7ffd49a0ab6a2
Tobias Leich <email@froggs.de>
2012-03-12 19:42:12 +0100

    fixed handling of joysticks

--- lib/Games/PangZero/Joystick.pm
+++ lib/Games/PangZero/Joystick.pm
@@ -7,12 +7,12 @@ use vars qw(@Games::PangZero::Joysticks @Games::PangZero::JoystickButtons);
 sub InitJoystick {
   my ($numJoysticks, $joystick, $numButtons, $i);
 
-  $numJoysticks = &SDL::Joystick::num_joysticks();
-  for ($i = 0; $i < $numJoysticks; ++$i) {
-    print STDERR "Found joystick " , $i+1 , ": " , &SDL::Joystick::open($i), "\n";
-    $joystick = &SDL::Joystick::open($i);
+  $numJoysticks = SDL::Joystick::num_joysticks();
+  for ($i = 0; $i < $numJoysticks; $i++) {
+    print STDERR "Found joystick " , $i+1 , ": " , SDL::Joystick::name($i), "\n";
+    $joystick   = SDL::Joystick->new($i);
     next unless $joystick;
-    $numButtons = &SDL::Joystick::open($joystick);
+    $numButtons = SDL::Joystick::num_buttons($joystick);
     next unless $numButtons;
     push @Games::PangZero::Joysticks, $joystick;
     push @Games::PangZero::JoystickButtons, $numButtons;
@@ -25,9 +25,8 @@ sub ReadJoystick {
   my ($i, $button, $buttonPressed);
 
   $i = 0;
-  foreach my $joystick (@Joysticks) {
-    ++$i;
-    my $axis = &SDL::Joystick::get_axis($joystick, 0);
+  foreach my $joystick (@Games::PangZero::Joysticks) {
+    my $axis = SDL::Joystick::get_axis($joystick, 0);
     if ($axis <= -10000) {
       $Games::PangZero::Events{"L$i"} = $Games::PangZero::MenuEvents{LEFT} = 1 unless $Games::PangZero::Keys{"L$i"};
       $Games::PangZero::Keys{"L$i"} = 1;
@@ -40,8 +39,9 @@ sub ReadJoystick {
       $Games::PangZero::Keys{"L$i"} = 0;
       $Games::PangZero::Keys{"R$i"} = 0;
     }
+
     if ($readBothAxes) {
-      $axis = &SDL::Joystick::get_axis($joystick, 1);
+      $axis = SDL::Joystick::get_axis($joystick, 1);
       if ($axis <= -10000) {
         $Games::PangZero::Events{"U$i"} = $Games::PangZero::MenuEvents{UP} = 1 unless $Games::PangZero::Keys{"U$i"};
         $Games::PangZero::Keys{"U$i"} = 1;
@@ -55,16 +55,19 @@ sub ReadJoystick {
         $Games::PangZero::Keys{"U$i"} = 0;
       }
     }
+
     $buttonPressed = 0;
-    for ($button = 0; $button < $JoystickButtons[$i-1]; ++$button) {
-      if (&SDL::Joystick::get_button($joystick, $button)) {
-        $buttonPressed = 1; last;
+    for ($button = 0; $button < $Games::PangZero::JoystickButtons[$i]; $button++) {
+      if (SDL::Joystick::get_button($joystick, $button)) {
+        $buttonPressed = 1;
+        last;
       }
     }
     if ($buttonPressed and not $Games::PangZero::Keys{"B$i"}) {
         $Games::PangZero::Events{"B$i"} = $Games::PangZero::MenuEvents{BUTTON} = 1;
     }
     $Games::PangZero::Keys{"B$i"} = $buttonPressed;
+    $i++;
   }
 }
 
